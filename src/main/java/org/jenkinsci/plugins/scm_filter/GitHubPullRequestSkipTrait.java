2784546c05f5ed301b3c89ce91a4e7c8668126e3
aloremia-freedom <aemielvin.loremia@freedom.tm>
2018-01-04 11:40:07 +0800

    Escape the '-' in the search regex

--- src/main/java/org/jenkinsci/plugins/scm_filter/GitHubPullRequestSkipTrait.java
+++ src/main/java/org/jenkinsci/plugins/scm_filter/GitHubPullRequestSkipTrait.java
@@ -97,7 +97,7 @@ public class GitHubPullRequestSkipTrait extends SCMSourceTrait {
                 int prNumber = Integer.parseInt(scmHead.getName().substring(3));
 
                 String prTitle = repository.getPullRequest(prNumber).getTitle();
-                return prTitle.matches("(?i).*\\[(wip|ci[- _]skip|skip[- _]ci)\\].*");
+                return prTitle.matches("(?i).*\\[(wip|ci[\\- _]skip|skip[\\- _]ci)\\].*");
             }
 
             return false;
